2818a2f1970682671af469a176f84c93b011eb12
IanW-99 <66699279+IanW-99@users.noreply.github.com>
2022-01-26 17:05:24 -0500

    top250shows complete

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+import json
+import secrets
+import requests
+
+
 def main():
-    print("Hello World!")
+    getTop250Tv()
+
+
+def getTop250Tv():
+    response = requests.get(f"https://imdb-api.com/en/API/Top250TVs/{secrets.imbdKey}")
+    text = json.dumps(response.json(), sort_keys=True, indent=2)
+    with open('top250.txt', 'w') as t:
+        t.write(text)
 
 
 main()
