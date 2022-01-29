6983f86d37efe80e5b548e202b9d55b02ad47dd2
Ian Washburn <iwashburn99@gmail.com>
2022-01-29 14:04:24 -0500

    top250shows fixed output

--- main.py
+++ main.py
@@ -9,9 +9,12 @@ def main():
 
 def getTop250Tv():
     response = requests.get(f"https://imdb-api.com/en/API/Top250TVs/{secrets.imbdKey}")
-    text = json.dumps(response.json(), sort_keys=True, indent=2)
-    with open('top250.txt', 'w') as t:
-        t.write(text)
+    json_data = response.json()
+
+    with open('top250.txt', 'w') as f:
+        for i in json_data['items']:
+            output_data = f'{i["rank"]}) {i["fullTitle"]} \n \t id: {i["id"]} \n'
+            f.write(output_data)
 
 
 main()
