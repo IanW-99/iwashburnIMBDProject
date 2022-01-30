c845cd3043a08a7846a58f05ee89f8564f5c0052
Ian Washburn <iwashburn99@gmail.com>
2022-01-30 16:31:34 -0500

    sprint 1 completed

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import requests
 def main():
     top250Data = getTop250Tv()
     showIDs = getShowID(top250Data)
-    rankingData = getRankings(showIDs)
-    writeToOutput(rankingData, top250Data)
+    ratingData = getRatings(showIDs)
+    writeToOutput(ratingData, top250Data)
 
 
 def getTop250Tv():
@@ -18,26 +18,30 @@ def getTop250Tv():
 def getShowID(top250Data):
     showIDs = []
     for i in top250Data["items"]:
-        if i["rank"] == 1 or 50 or 100 or 200:
+        if int(i["rank"]) == 1 or int(i["rank"]) == 50 or int(i["rank"]) == 100 or int(i["rank"]) == 200:
             showID = i["id"]
             showIDs.append(showID)
-            print(showID)
     return showIDs
 
 
-def getRankings(showIDs):
+def getRatings(showIDs):
     user_rankings = []
     for showID in showIDs:
         response = requests.get(f"https://imdb-api.com/en/API/UserRatings/{secrets.imbdKey}/{showID}")
         json_data = response.json()
         user_rankings.append(json_data)
+    wotRatings = requests.get(f"https://imdb-api.com/en/API/UserRatings/{secrets.imbdKey}/tt0331080")
+    wot_data = wotRatings.json()
+    user_rankings.append(wot_data)
+
     return user_rankings
 
 
-def writeToOutput(rankingData, top250Data):
+def writeToOutput(ratingData, top250Data):
     with open('top250.txt', 'w') as f:
-        for i in rankingData:
-            print(i)
+        for i in ratingData:
+            output_data = f'{i} \n'
+            f.write(output_data)
 
         for i in top250Data['items']:
             output_data = f'{i["rank"]}) {i["fullTitle"]} \n \t id: {i["id"]} \n'
