72e583d07ab15baa782186e27b5afaa721681bb1
Ian Washburn <iwashburn99@gmail.com>
2022-02-16 22:31:42 -0500

    fixed directory issues

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,12 +7,12 @@ def test_getTop250Tv():
 
 
 def test_dbCreation_and_dbFilling():
-    testHeadlineDict = {'t0000000': {'title': 'test', 'fullTitle': 'test_show', 'year': '2022',
+    testHeadlineDict = {'t0000000': {'rank': 1, 'title': 'test', 'fullTitle': 'test_show', 'year': '2022',
                         'crew': 'Ian Washburn, John Santore', 'imdbRating': '100', 'imdbRatingCount': '1'}}
     test_databaseName = 'test_db.db'
     conn, curs = main.dbConnect(test_databaseName)
     main.createDataBase(curs)
-    main.fillHeadlineData(conn, curs, testHeadlineDict)
-    curs.execute('''SELECT title FROM headlineData WHERE id = ?''', ('t0000000',))
+    main.fillTop250TvData(conn, curs, testHeadlineDict)
+    curs.execute('''SELECT title FROM top250TvData WHERE id = ?''', ('t0000000',))
     title = curs.fetchone()[0]
     assert title == 'test'
