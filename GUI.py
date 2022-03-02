e4970088989d83216f8b0ea978d54b3414fc5c0c
Ian Washburn <iwashburn99@gmail.com>
2022-03-02 18:01:10 -0500

    'Tv Data' button in DataVisualization menu now opens new table window

--- GUI.py
+++ GUI.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-from PyQt5.QtWidgets import QMainWindow, QPushButton
+from PyQt5.QtWidgets import QMainWindow, QPushButton, QTableWidget, QTableWidgetItem
 import api_interaction
 
 
@@ -35,7 +35,7 @@ class MainWindow(QMainWindow):
 
     def open_data_visualization(self):
         self.hide()
-        self.DataVisualization.show()
+        self.data_visualization.show()
 
     def update_data(self):
         api_interaction.data_grab_and_store(self.conn, self.curs)
@@ -51,3 +51,64 @@ class DataVisualization(QMainWindow):
         self.setWindowTitle('iwashburnIMDBProject/DataVisualization')
         self.setGeometry(400, 200, 1000, 750)
         self.setFixedSize(1000, 750)
+
+        self.tv_data_open = False
+        self.tv_data = QPushButton(self)
+        self.movie_data_open = False
+        self.movie_data = QPushButton(self)
+
+        self.tv_data.setText('Tv Data')
+        self.tv_data.resize(150, 50)
+        self.tv_data.move(50, 200)
+        self.tv_data.clicked.connect(self.open_tv_data)
+
+        self.movie_data.setText('Movie Data')
+        self.movie_data.resize(150, 50)
+        self.movie_data.move(50, 250)
+        # self.movie_data.clicked.connect(self.open_movie_data)
+
+        self.tv_table_window = 0
+        self.movie_table_window = 0
+
+    def open_tv_data(self):
+        test_dict = {1: {'rank': '2', 'rankChange': '+3', 'title': 'Test Show'}, 2: {'rank': '1', 'rankChange': '-6',
+                                                                                     'title': 'Test Show2'}}
+        tv_table = TvTable(test_dict)
+        self.tv_table_window = TableWindow(tv_table)
+        self.tv_table_window.show()
+
+
+class TableWindow(QMainWindow):
+    def __init__(self, table: QTableWidget):
+        super().__init__()
+        self.table = table
+        self.setWindowTitle(self.table.title)
+        self.setGeometry(500, 500, 400, 600)
+        self.setFixedSize(400, 600)
+        self.setCentralWidget(self.table)
+
+
+class TvTable(QTableWidget):
+    def __init__(self, tv_data: dict):
+        super().__init__()
+        self.tv_data = tv_data
+        self.title = 'Most Popular Tv Table'
+        self.width = 400
+        self.height = 600
+        self.setup_ui()
+
+    def setup_ui(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(0, 0, self.width, self.height)
+
+        self.create_tv_table()
+
+    def create_tv_table(self):
+        self.setRowCount(len(self.tv_data))
+        self.setColumnCount(3)
+
+        for i in self.tv_data:
+            self.setItem(i-1, 0, QTableWidgetItem(self.tv_data[i]['rank']))
+            self.setItem(i-1, 1, QTableWidgetItem(self.tv_data[i]['rankChange']))
+            self.setItem(i-1, 2, QTableWidgetItem(self.tv_data[i]['title']))
+        self.move(0, 0)
