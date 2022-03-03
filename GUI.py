2b7e7d5f078d85c77ed1ef9de4bfb26fe105ac1a
Ian Washburn <iwashburn99@gmail.com>
2022-03-02 22:39:37 -0500

    Foundation of sorting methods

--- GUI.py
+++ GUI.py
@@ -1,6 +1,5 @@
 import sqlite3
-
-from PyQt5.QtWidgets import QMainWindow, QPushButton, QTableWidget, QTableWidgetItem
+from PyQt5.QtWidgets import QMainWindow, QPushButton, QTableWidget, QTableWidgetItem, QWidget, QVBoxLayout
 import api_interaction
 
 
@@ -20,17 +19,17 @@ class MainWindow(QMainWindow):
 
     def setup_ui(self):
         self.setWindowTitle('iwashburnIMDBProject')
-        self.setGeometry(400, 200, 1000, 750)
-        self.setFixedSize(1000, 750)
+        self.setGeometry(400, 400, 400, 300)
+        self.setFixedSize(400, 150)
 
         self.update_data_button.setText("Update Data")
         self.update_data_button.resize(150, 50)
-        self.update_data_button.move(50, 200)
+        self.update_data_button.move(50, 50)
         self.update_data_button.clicked.connect(self.update_data)
 
         self.data_visualization_menu_button.setText("Data Visualization")
         self.data_visualization_menu_button.resize(150, 50)
-        self.data_visualization_menu_button.move(50, 250)
+        self.data_visualization_menu_button.move(200, 50)
         self.data_visualization_menu_button.clicked.connect(self.open_data_visualization)
 
     def open_data_visualization(self):
@@ -49,8 +48,8 @@ class DataVisualization(QMainWindow):
         self.previous_window = previous_window
         super(DataVisualization, self).__init__()
         self.setWindowTitle('iwashburnIMDBProject/DataVisualization')
-        self.setGeometry(400, 200, 1000, 750)
-        self.setFixedSize(1000, 750)
+        self.setGeometry(400, 400, 400, 150)
+        self.setFixedSize(400, 150)
 
         self.tv_data_open = False
         self.tv_data = QPushButton(self)
@@ -59,12 +58,12 @@ class DataVisualization(QMainWindow):
 
         self.tv_data.setText('Tv Data')
         self.tv_data.resize(150, 50)
-        self.tv_data.move(50, 200)
+        self.tv_data.move(50, 50)
         self.tv_data.clicked.connect(self.open_tv_data)
 
         self.movie_data.setText('Movie Data')
         self.movie_data.resize(150, 50)
-        self.movie_data.move(50, 250)
+        self.movie_data.move(200, 50)
         # self.movie_data.clicked.connect(self.open_movie_data)
 
         self.tv_table_window = 0
@@ -88,9 +87,19 @@ class TableWindow(QMainWindow):
         super().__init__()
         self.table = table
         self.setWindowTitle(self.table.title)
-        self.setGeometry(500, 500, 400, 600)
+        self.setGeometry(400, 400, 500, 600)
         self.setFixedSize(500, 600)
-        self.setCentralWidget(self.table)
+
+        self.sort_by_rank = QPushButton('Sort by Rank')
+        self.sort_by_rank.clicked.connect(table.sort_by_rank)
+
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.sort_by_rank)
+        self.layout.addWidget(self.table)
+
+        self.widget = QWidget()
+        self.widget.setLayout(self.layout)
+        self.setCentralWidget(self.widget)
 
 
 class TvTable(QTableWidget):
@@ -119,12 +128,20 @@ class TvTable(QTableWidget):
         self.setColumnWidth(3, 100)
         self.setHorizontalHeaderLabels(self.column_labels)
         self.verticalHeader().setVisible(False)
+        self.setSortingEnabled(False)
+        self.fill_table(self.tv_data.keys())
+        self.move(0, 0)
 
+    def fill_table(self, keys):
         i = 0
-        for key in self.tv_data:
+        for key in keys:
             self.setItem(i, 0, QTableWidgetItem(self.tv_data[key]['rank']))
+            print('got here')
             self.setItem(i, 1, QTableWidgetItem(self.tv_data[key]['rankUpDown']))
             self.setItem(i, 2, QTableWidgetItem(self.tv_data[key]['title']))
             self.setItem(i, 3, QTableWidgetItem(key))
             i += 1
-        self.move(0, 0)
+
+    def sort_by_rank(self):
+        sorted_keys = sorted(self.tv_data, key=lambda x: (self.tv_data[x]['rank']))
+        self.fill_table(sorted_keys)
